d991344d2af867d809412c750183c6df01932dd0
rokydas <droky136@gmail.com>
2022-04-08 20:16:28 +0600

    sign up ui done

--- app/src/main/java/com/example/trello_clone/presentation/RegistrationScreen.kt
+++ app/src/main/java/com/example/trello_clone/presentation/RegistrationScreen.kt
@@ -1,22 +1,36 @@
 package com.example.trello_clone.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.trello_clone.ui.theme.PrimaryColor
+import com.example.trello_clone.ui.theme.SecondaryColor
+
+inline fun Modifier.noRippleClickable(crossinline onClick: ()->Unit): Modifier = composed {
+    clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
+    }
+}
 
 @Composable
 fun RegistrationScreen(navController: NavController) {
@@ -63,10 +77,92 @@ fun RegistrationScreen(navController: NavController) {
 
         Box(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(horizontal = 20.dp, vertical = 10.dp)
+                .shadow(5.dp, shape = RoundedCornerShape(10.dp))
+                .background(Color.White)
+                .padding(vertical = 15.dp, horizontal = 25.dp)
+
         ) {
 
+            var name by rememberSaveable { mutableStateOf("") }
+            var email by rememberSaveable { mutableStateOf("") }
+            var password by rememberSaveable { mutableStateOf("") }
+
+            Column {
+                TextField(
+                    modifier = Modifier
+                        .background(Color.White),
+                    value = name,
+                    onValueChange = { name = it },
+                    placeholder = { Text("Name") },
+                    maxLines = 1,
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.White,
+                        cursorColor = Color.Gray,
+                        focusedIndicatorColor = Color.Gray
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                TextField(
+                    modifier = Modifier
+                        .background(Color.White),
+                    value = email,
+                    onValueChange = { email = it },
+                    placeholder = { Text("Email") },
+                    maxLines = 1,
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.White,
+                        cursorColor = Color.Gray,
+                        focusedIndicatorColor = Color.Gray
+                    ),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                TextField(
+                    modifier = Modifier
+                        .background(Color.White),
+                    value = password,
+                    onValueChange = { password = it },
+                    placeholder = { Text("Password") },
+                    maxLines = 1,
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.White,
+                        cursorColor = Color.Gray,
+                        focusedIndicatorColor = Color.Gray
+                    ),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                val gradient = Brush.horizontalGradient(listOf(SecondaryColor, PrimaryColor))
+
+                Box(
+                    modifier = Modifier
+                        .noRippleClickable() {
+                            // sign up
+                        }
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .background(gradient)
+                        .padding(8.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = "SIGN UP",
+                        color = Color.White,
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+            }
         }
 
     }
 }
+
+
